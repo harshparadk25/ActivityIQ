4e96a388005070ac6b40886e8c2f26cdeac1d1b5
harshparadkar25 <harshparadkar25@gmail.com>
2026-01-14 01:25:19 +0530

    backend fixed

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors(
-  {origin: process.env.FRONTEND_URL }
-));
-app.use(express.json());
+app.use(cors({
+  origin: [
+    process.env.FRONTEND_URL,
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
+
+app.use(express.json({ limit: "10mb" }));
+
 
 app.get("/api/health", (req, res) => {
   res.json({ status: "OK", message: "ActivityIQ Backend Running" });
@@ -27,4 +33,7 @@ app.use("/api/analytics", analyticsRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
