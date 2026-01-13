a3e780258f6d18d2f0008a9ba0789ff01dead89e
harshparadkar25 <harshparadkar25@gmail.com>
2026-01-14 01:30:32 +0530

    health api

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,9 @@ app.use("/api/analytics", analyticsRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
